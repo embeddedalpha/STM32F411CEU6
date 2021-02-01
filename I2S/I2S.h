ef6fcccc8219188d60c96b3cf37df3ffc39c78ed
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-01 22:09:50 +0530

    Update I2S.h

--- I2S/I2S.h
+++ I2S/I2S.h
@@ -1,31 +1,12 @@
 #ifndef		_I2S_H_
 #define 	_I2S_H_
 
-
-
 #include "stm32f411xe.h"
 
 
-#define Philips_Standard	0
-#define Left_Justified		1
-#define Right_Justified		2
-#define PCM_Standard			3
 
-#define Data_Length_16Bit		0
-#define Data_Length_24Bit		1
-#define Data_Length_32Bit		2
-
-#define Slave_Transmit		0
-#define Slave_Receive			1
-#define Master_Transmit		2
-#define Master_Receive		3
-
-
-void I2S_Init(SPI_TypeDef *I2S, 
-	            uint8_t mode, 
-              uint8_t standard, 
-              uint8_t data_length, 
-              uint8_t channel_length);
+void I2S1_Master__Init(void);
+void I2S2_Master_Init(void);
 
 
 
@@ -40,5 +21,4 @@ void I2S_Init(SPI_TypeDef *I2S,
 
 
 #endif
-
 
