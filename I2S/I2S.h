b42853884397aecb3983e02c6b70f349a09f8a31
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-03 20:59:20 +0530

    Update I2S.h

--- I2S/I2S.h
+++ I2S/I2S.h
@@ -12,6 +12,9 @@ void I2S4_Master_Init(void);
 void I2S5_Master__Init(void);
 
 
+void I2S_Master_Send_Data_to_Left_Channel(SPI_TypeDef *I2S, uint8_t *data);
+void I2S_Master_Send_Data_to_Right_Channel(SPI_TypeDef *I2S, uint8_t *data);
+
 
 
 
