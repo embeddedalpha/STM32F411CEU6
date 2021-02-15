4d6a4249e807a8babeb80ac446e7a8803b386c18
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-15 22:47:17 +0530

    Update GPIO.h

--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -22,13 +22,23 @@
 #define ALT_PushPull_Output_OpenDrain_PullUP   0x5C
 #define ALT_PushPull_Output_OpenDrain_PullDown 0x5C
 
+#define Input_Floating	0x00
+#define Input_PullUP		0x01
+#define Input_PullDown	0x02
+#define Analog_Pin      0x60
 
 
 void GPIO_Init(GPIO_TypeDef *GPIO,uint8_t pin, uint8_t config);
 
 
-
-
+void ADC_Pin_Init(uint8_t pin);
+void SPI_Pin_Init(SPI_TypeDef *SPI);
+void I2S_Pin_Init(SPI_TypeDef *SPI);
+void I2C_Pin_Init(I2C_TypeDef *I2C);
+void USART_Pin_Init(USART_TypeDef *USART);
+void UART_Pin_Init(USART_TypeDef *USART);
+void TIM_Pin_Init(TIM_TypeDef *TIM);
+void SDIO_Pin_Init(SDIO_TypeDef *sdio);
 
 
 
@@ -37,4 +47,3 @@ void GPIO_Init(GPIO_TypeDef *GPIO,uint8_t pin, uint8_t config);
 
 
 #endif
-
