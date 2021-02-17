3b9e5ffee519962c1c2bbbb950e83a411d8f4b1d
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-02-17 21:32:44 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,1 +1,33 @@
+#ifndef _MAIN_
+#define _MAIN_
+
+
+
+
+#include "stm32f411xe.h"
+
+#define SPI_FULL_DUPLEX_MASTER       0
+#define SPI_FULL_DUPLEX_SLAVE        1
+#define SPI_HALF_DUPLEX_MASTER       2
+#define SPI_HALF_DUPLEX_SLAVE        3
+#define SPI_RECEIVE_ONLY_MASTER      4
+#define SPI_RECEIVE_ONLY_SLAVE       5
+#define SPI_TRANSMIT_ONLY_MASTER     6
+#define SPI_TRANSMIT_ONLY_SLAVE      7
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
 
