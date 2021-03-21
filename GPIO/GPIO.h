c5b661a1566aff861cf3c8acaae6e742676d3748
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-03-21 21:55:50 +0530

    Update GPIO.h

--- GPIO/GPIO.h
+++ GPIO/GPIO.h
@@ -11,24 +11,24 @@
 #include "stm32f4xx.h"
 #include "stm32f411xe.h"
 
-#define GEN_PUSH_PULL_OUT	        0b0101100
-#define GEN_PUSH_PULL_OUT_PULL_UP	0b0101101
-#define GEN_PUSH_PULL_OUT_PULL_DW	0b0101110
-#define GEN_PUSH_PULL_OUT	        0b0101100
-#define GEN_PUSH_PULL_OUT_PULL_UP	0b0101101
-#define GEN_PUSH_PULL_OUT_PULL_DW	0b0101110
+#define GEN_PUSH_PULL_OUT	        0b01011000
+#define GEN_PUSH_PULL_OUT_PULL_UP	0b01011010
+#define GEN_PUSH_PULL_OUT_PULL_DW	0b01011100
+#define GEN_OPEN_DRAIN_OUT	        0b01011000
+#define GEN_OPEN_DRAIN_OUT_PULL_UP	0b01011010
+#define GEN_OPEN_DRAIN_OUT_PULL_DW	0b01011100
 
-#define ALT_PUSH_PULL_OUT	        0b1001100
-#define ALT_PUSH_PULL_OUT_PULL_UP	0b1001101
-#define ALT_PUSH_PULL_OUT_PULL_DW	0b1001110
-#define ALT_PUSH_PULL_OUT	        0b1001100
-#define ALT_PUSH_PULL_OUT_PULL_UP	0b1001101
-#define ALT_PUSH_PULL_OUT_PULL_DW	0b1001110
+#define ALT_PUSH_PULL_OUT	        0b10011000
+#define ALT_PUSH_PULL_OUT_PULL_UP	0b10011010
+#define ALT_PUSH_PULL_OUT_PULL_DW	0b10011100
+#define ALT_OPEN_DRAIN_OUT	        0b10011000
+#define ALT_OPEN_DRAIN_OUT_PULL_UP	0b10011010
+#define ALT_OPEN_DRAINL_OUT_PULL_DW	0b10011100
 
-#define FLOAT_PUSH_PULL_IN	      0b0000000
-#define INPUT_PULL_UP	            0b0000001
-#define INPUT_PULL_DW	            0b0000010
-#define INPUT_ANALOG      	      0b1100000
+#define FLOAT_PUSH_PULL_IN	        0b00000000
+#define INPUT_PULL_UP	            0b00000010
+#define INPUT_PULL_DW	            0b00000100
+#define INPUT_ANALOG      	        0b11000000
 
 
 #define Rising_Edge     1
@@ -43,5 +43,14 @@ void GPIO_Pin_Interrupt_Setup(GPIO_TypeDef *port, int pin, int type);
 void GPIO_Interrupt_Enable(int pin, int priority, IRQn_Type irqnum);
 
 
+void I2C1_Pin_Setup(void);
+void I2C2_Pin_Setup(void);
+void I2C3_Pin_Setup(void);
+
+void SPI1_Pin_Setup(void);
+void SPI2_Pin_Setup(void);
+void SPI3_Pin_Setup(void);
+void SPI4_Pin_Setup(void);
+void SPI5_Pin_Setup(void);
 
 #endif /* GPIO_GPIO_H_ */
