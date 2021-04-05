6480332a60119c9cfaba900eebe817b41d34ec43
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-04-05 22:54:51 +0530

    Update USART.c

--- USART/USART.c
+++ USART/USART.c
@@ -1,1 +1,46 @@
+/*
+ * USART.c
+ *
+ *  Created on: 05-Apr-2021
+ *      Author: Kunal
+ */
+
+
+
+#include "USART.h"
+
+
+void USART_Init(USART_Config USART)
+{
+	if(USART.USART == USART1)
+	{
+		USART1_Pin_Setup();
+	}
+	if(USART.USART == USART2)
+	{
+		USART2_Pin_Setup();
+	}
+
+}
+
+void USART_TX(USART_Config USART, uint8_t data)
+{
+
+}
+
+uint8_t USART_RX(USART_Config USART)
+{
+
+}
+
+void USART_TX_HFC(USART_Config USART, uint8_t data)
+{
+
+}
+
+uint8_t USART_RX_HFC(USART_Config USART)
+{
+
+}
+
 
