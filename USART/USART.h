aedd15cb46fe74fa7c33edf821e37cd708c74f95
Blackshield Engineering <kunalsalvi63@gmail.com>
2021-04-05 22:30:53 +0530

    Update USART.h

--- USART/USART.h
+++ USART/USART.h
@@ -1,1 +1,42 @@
+/*
+ * USART.h
+ *
+ *  Created on: 05-Apr-2021
+ *      Author: Kunal
+ */
+
+#ifndef USART_USART_H_
+#define USART_USART_H_
+
+#include "stm32f4xx.h"
+#include "stm32f411xe.h"
+#include "stdbool.h"
+#include "GPIO/GPIO.h"
+
+#define Baud_2400		  4375
+#define Baud_9600		  1093.75
+#define Baud_19200		546.875
+#define Baud_38400		273.5
+#define Baud_57600		182.25
+#define Baud_115200		91.125
+#define Baud_230400		45.625
+#define Baud_460800		22.75
+#define Baud_921600		11.375
+#define Baud_1792000	5.875
+#define Baud_1843200	5.75
+#define Baud_3584000	2.875
+#define Baud_3686400	2.875
+#define Baud_7168000	1.5
+#define Baud_7372800	1.375
+#define Baud_9000000	1.125
+#define Baud_10500000	1
+
+
+struct USART_Config{
+
+};
+
+
+
+#endif /* USART_USART_H_ */
 
