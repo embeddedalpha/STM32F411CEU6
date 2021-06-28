ca88ed285ff05ccd26d08a13af9e208a3d426dcf
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-28 23:14:07 +0530

    Update I2S.h

--- I2S/I2S.h
+++ I2S/I2S.h
@@ -1,29 +1,25 @@
-#ifndef		_I2S_H_
-#define 	_I2S_H_
+/*
+ * I2S.h
+ *
+ *  Created on: 28-Jun-2021
+ *      Author: Kunal
+ */
+
+#ifndef I2S_I2S_H_
+#define I2S_I2S_H_
 
 #include "stm32f411xe.h"
+#include "stm32f4xx.h"
+#include "stdbool.h"
 
 
 
-void I2S1_Master__Init(void);
-void I2S2_Master_Init(void);
-void I2S3_Master__Init(void);
-void I2S4_Master_Init(void);
-void I2S5_Master__Init(void);
+void I2S_Mater_Init(SPI_TypeDef *I2S, bool tx_or_rx);
 
+void I2S_Master_Shut_Down(SPI_TypeDef *I2S);
 
-void I2S_Master_Send_Data(SPI_TypeDef *I2S, uint32_t *r_channel,  uint32_t *l_channel , int length);
+uint32_t I2S_Master_Receive(SPI_TypeDef *I2S);
 
+void I2S_Master_Transmit(SPI_TypeDef *I2S, uint16_t tx);
 
-
-
-
-
-
-
-
-
-
-
-#endif
-
+#endif /* I2S_I2S_H_ */
