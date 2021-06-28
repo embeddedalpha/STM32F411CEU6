ea326ed1444a3f9725630980409d1c2760605e63
Kunal Salvi <kunalsalvi63@gmail.com>
2021-06-28 23:12:05 +0530

    Update I2S.c

--- I2S/I2S.c
+++ I2S/I2S.c
@@ -24,9 +24,41 @@ void I2S_Mater_Init(SPI_TypeDef *I2S, bool tx_or_rx)
 	I2S->I2SCFGR &= ~SPI_I2SCFGR_I2SSTD; //phillips standard
 	I2S->I2SCFGR |= SPI_I2SCFGR_DATLEN_1; //24 bit data
 	I2S->I2SCFGR |= SPI_I2SCFGR_CHLEN; //32 bit per channel
-	I2S->I2SCFGR |= SPI_I2SCFGR_CKPOL;
+	I2S->I2SCFGR &= ~SPI_I2SCFGR_CKPOL;
 	I2S->I2SPR |= SPI_I2SPR_ODD | 12;
 	I2S->I2SCFGR |= SPI_I2SCFGR_I2SE; //Enable i2s peripheral
 }
 
 
+void I2S_Master_Shut_Down(SPI_TypeDef *I2S)
+{
+	while(READ_BIT(I2S->SR,SPI_SR_BSY) == 1 && READ_BIT(I2S->SR,SPI_SR_TXE) == 0)
+	{
+
+	}
+	I2S->I2SCFGR &= ~SPI_I2SCFGR_I2SE;
+
+}
+
+uint32_t I2S_Master_Receive(SPI_TypeDef *I2S)
+{
+	uint16_t left,right;
+	uint32_t packet;
+	while(!(I2S -> SR & SPI_SR_RXNE));
+	if((I2S->SR & SPI_SR_CHSIDE) == 0){
+		left =  I2S -> DR;
+	}
+	else{
+		right =  I2S -> DR;
+	}
+
+	packet = left << 16 | right;
+	return packet;
+}
+
+
+void I2S_Master_Transmit(SPI_TypeDef *I2S, uint16_t tx)
+{
+	I2S->DR = tx;
+	while(!(I2S -> SR & SPI_SR_TXE));
+}
