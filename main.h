b62af1f59783471718ae4610887ca767120d8fce
Kunal Salvi <kunalsalvi63@gmail.com>
2021-07-11 21:48:17 +0530

    Update main.h

--- main.h
+++ main.h
@@ -1,17 +1,26 @@
-#ifndef _MAIN_
-#define _MAIN_
+/*
+ * main.h
+ *
+ *  Created on: 09-Jul-2021
+ *      Author: Kunal
+ */
 
-
-
+#ifndef MAIN_H_
+#define MAIN_H_
 
 #include "stm32f4xx.h"
 #include "stm32f411xe.h"
+#include "stdbool.h"
+#include "stdint.h"
 
+__STATIC_INLINE void MCU_Clock_Setup(void)
+{
+	uint8_t pll_m = 25;
+	uint8_t pll_n = 192;
+	uint8_t pll_p = 0;
+	uint8_t pll_q = 4;
 
-
-__STATIC_INLINE void MCU_Clock_Setup(void){
-  
- 
+	RCC->PLLCFGR = 0x00000000;
 
 	RCC -> CR |= RCC_CR_HSEON;
 	while(!(RCC -> CR & RCC_CR_HSERDY)){}
@@ -19,43 +28,63 @@ __STATIC_INLINE void MCU_Clock_Setup(void){
 	PWR ->CR |= PWR_CR_VOS;
 	FLASH -> ACR |= FLASH_ACR_ICEN | FLASH_ACR_PRFTEN | FLASH_ACR_DCEN |
 			FLASH_ACR_LATENCY_3WS;
-	RCC -> CR |= RCC_CR_PLLON;
 	RCC -> CFGR |= RCC_CFGR_HPRE_DIV1;
 	RCC -> CFGR |= RCC_CFGR_PPRE1_DIV2;
 	RCC -> CFGR |= RCC_CFGR_PPRE2_DIV1;
-	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLSRC_HSE;
-	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLM_4 | RCC_PLLCFGR_PLLM_3 | RCC_PLLCFGR_PLLM_0;
-	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLN_7 | RCC_PLLCFGR_PLLN_6;
-	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLP_1;
-	RCC -> PLLCFGR |= RCC_PLLCFGR_PLLQ_2;
-	RCC -> CR |= RCC_CR_PLLI2SON;
-	while(!(RCC->CR & RCC_CR_PLLI2SRDY)){}
+	RCC->PLLCFGR |= (pll_q << 24) | (pll_p << 16) | (pll_n << 6) | (pll_m << 0);
+	RCC ->PLLCFGR |= 1 << 22;
+	RCC -> APB2ENR |= 0x4000;
+	RCC -> CR |= RCC_CR_PLLON;
+	while(!(RCC->CR & RCC_CR_PLLRDY)){}
+//	RCC -> CR |= RCC_CR_PLLI2SON;
+//	while(!(RCC->CR & RCC_CR_PLLI2SRDY)){}
 	RCC -> CFGR |= RCC_CFGR_SW_PLL;
 	while((RCC -> CFGR & RCC_CFGR_SWS_PLL) != RCC_CFGR_SWS_PLL);
+SystemCoreClockUpdate();
+SysTick_Config(SystemCoreClock/1000);
 
- 
-  
+
+
+
 }
 
-
-
-
-__STATIC_INLINE void I2S_Clock_Setup(void)
+__STATIC_INLINE uint32_t Delay_Config(void)
 {
-	RCC -> PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SM_4;
-	RCC -> PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SN_7 | RCC_PLLI2SCFGR_PLLI2SN_6;
-	RCC -> PLLI2SCFGR |= RCC_PLLI2SCFGR_PLLI2SR_2 | RCC_PLLI2SCFGR_PLLI2SR_0;
-	RCC -> CFGR &= ~RCC_CFGR_I2SSRC;
+
+	SysTick->CTRL = 0;
+	SysTick->LOAD = 0x00FFFFFF;
+	SysTick->VAL = 0;
+	SysTick->CTRL |= 5;
+	return (0UL);                                                     /* Function successful */
+}
+
+__STATIC_INLINE uint32_t Delay_us(unsigned long us)
+{
+
+	SysTick->LOAD = (uint32_t)(us * (1/1000000) * SystemCoreClock);
+	SysTick->VAL = 0;
+	while((SysTick->CTRL & 0x00010000) == 0);
+	return (0UL);                                                     /* Function successful */
+}
+
+__STATIC_INLINE uint32_t Delay_ms(unsigned long ms)
+{
+	SysTick->LOAD = (uint32_t)(ms * (1/1000) * SystemCoreClock);
+	SysTick->VAL = 0;
+	while((SysTick->CTRL & 0x00010000) == 0);
+	return (0UL);                                                     /* Function successful */
 }
 
 
+__STATIC_INLINE uint32_t Delay_s(unsigned long s)
+{
+	s = s * 1000;
+	for (; s>0; s--)
+	{
+		Delay_ms(1);
+	}
+	return (0UL);
+}
 
 
-
-
-
-
-
-
-#endif
-
+#endif /* MAIN_H_ */
