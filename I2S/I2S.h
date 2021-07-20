88274c2bda17ecee1eff08c65454a44fa7805e10
Kunal Salvi <kunalsalvi63@gmail.com>
2021-07-20 23:30:49 +0530

    Update I2S.h

--- I2S/I2S.h
+++ I2S/I2S.h
@@ -12,9 +12,36 @@
 #include "stm32f4xx.h"
 #include "stdbool.h"
 
+#define channel_length_16_bit 0;
+#define channel_length_32_bit 1;
+
+#define data_length_16_bit 0;
+#define data_length_24_bit 1;
+#define data_length_32_bit 2;
+
+#define Philips_Standard 0;
+#define MSB_Justified_Standard 1;
+#define LSB_Justified_Standard 2;
+#define PCM_Standard 3;
+
+#define Slave_Transmit 0;
+#define Slave_Receive 1;
+#define Master_Transmit 2;
+#define Master_Receive 3;
+
+typedef struct I2S_Config
+{
+	SPI_TypeDef *I2S;
+	uint8_t mode;
+	uint8_t standard;
+	uint8_t data_length;
+	uint8_t channel_length;
+	int32_t audio_frequency;
+
+}I2S_Config;
 
 
-void I2S_Mater_Init(SPI_TypeDef *I2S, bool tx_or_rx);
+void I2C_Master_Init(I2S_Config I2S);
 
 void I2S_Master_Shut_Down(SPI_TypeDef *I2S);
 
