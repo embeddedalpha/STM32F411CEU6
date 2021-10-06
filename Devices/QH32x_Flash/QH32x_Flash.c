54408996e949731badaad4b56f8789537f29e7d0
Kunal Salvi <kunalsalvi63@gmail.com>
2021-10-06 22:21:41 +0530

    Update QH32x_Flash.c

--- Devices/QH32x_Flash/QH32x_Flash.c
+++ Devices/QH32x_Flash/QH32x_Flash.c
@@ -12,36 +12,53 @@
 
 static uint8_t QH32x_Read_SR1(void)
 {
-	uint8_t SR1;
+	uint8_t SR1[2];
 	SPI_CSS_Low(QH32);
-	SPI_Send_Data(QH32, 0x01); //SR1
-	SR1 = SPI_Receive_Data(QH32);
+	SPI_Send_Data(QH32, 0x05); //SR1
+	SR1[0] = SPI_Receive_Data(QH32);
+	SR1[1] = SPI_Receive_Data(QH32);
 	SPI_CSS_High(QH32);
-	return SR1;
+	return SR1[1];
 }
 
  static uint8_t QH32x_Read_SR2(void)
 {
-	uint8_t SR2;
+	uint8_t SR2[2];
 	SPI_CSS_Low(QH32);
 	SPI_Send_Data(QH32, 0x31); //SR2
-	SR2 = SPI_Receive_Data(QH32);
+	SR2[0] = SPI_Receive_Data(QH32);
+	SR2[1] = SPI_Receive_Data(QH32);
 	SPI_CSS_High(QH32);
-	return SR2;
+	return SR2[1];
 }
 
- static  void QH32x_Read_Busy_Bit(void)
+
+ static uint8_t QH32x_Read_Busy_Bit(void)
 {
-	 uint8_t a;
+	 uint8_t a,b;
 	 a = QH32x_Read_SR1();
-	 return (0x01 & a);
+	 b = ((0x01 & a) >> 0);
+	 if(b == 1)
+	 {
+		 return 1;
+	 }
+	 else{
+		 return 0;
+	 }
+
 }
 
  static bool QH32x_Read_Write_Enable_Latch(void)
  {
 	 uint8_t a;
 	 a = QH32x_Read_SR1();
-	 return (0x02 & a);
+	 if(((0x02 & a) >> 1) == 1)
+	 {
+		 return 1;
+	 }
+	 else{
+		 return 0;
+	 }
 
  }
 
@@ -61,28 +78,55 @@ static uint8_t QH32x_Read_SR1(void)
 
   }
 
- static  void QH32x_Suspend_Status_Bit(void)
+ static  uint8_t QH32x_Suspend_Status_Bit(void)
 {
 	 uint8_t a;
 	 a = QH32x_Read_SR2();
 	 return (0x80 & a);
 }
 
- static void QH32x_Write_SR2(void)
+ static void QH32x_Write_SR1(uint8_t data)
 {
+	 	SPI_CSS_Low(QH32);
+	 	SPI_Send_Data(QH32, 0x50); //Write SR volatile
+	 	SPI_CSS_High(QH32);
+
+	 	SPI_CSS_Low(QH32);
+	 	SPI_Send_Data(QH32, 0x01); //Write SR volatile
+	 	SPI_Send_Data(QH32, data); //Write SR volatile
+	 	SPI_CSS_High(QH32);
 
 }
+
+ static void QH32x_Flash_Write_Enable()
+ {
+	 do{
+ 	SPI_CSS_Low(QH32);
+ 	SPI_Send_Data(QH32, 0x06); //Write Enable
+ 	SPI_CSS_High(QH32);
+	 }
+ 	while(QH32x_Read_Write_Enable_Latch() == 0);
+
+ }
+
+ static void QH32x_Flash_Write_Disable()
+ {
+ 	SPI_CSS_Low(QH32);
+ 	SPI_Send_Data(QH32, Write_Disable); //Write Disable
+ 	SPI_CSS_High(QH32);
+ 	while(QH32x_Read_Write_Enable_Latch() == 1){}
+ }
 
 //******************************************** End **************************************************************
 
 
-bool QH32x_Init(SPI_TypeDef *SPI)
+void QH32x_Init(SPI_TypeDef *SPI)
 {
 	QH32.SPI = SPI1;
 	QH32.Frame = SPI_Frame_8Bit;
 	QH32.phase = 1;
 	QH32.polarity = 1;
-	QH32.frequency = 4;
+	QH32.frequency = 3;
 	QH32.mode = SPI_Master;
 	QH32.type = Full_Duplex_Master;
 	SPI_Init(QH32);
@@ -91,22 +135,124 @@ bool QH32x_Init(SPI_TypeDef *SPI)
 
 void QH32x_Reset()
 {
-
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, 0x66); //RESET ENABLE
+	SPI_Send_Data(QH32, 0x99); //SOFTWARE RESET
+	SPI_CSS_High(QH32);
 }
-void QH32x_Flash_Write_Enable()
+
+void QH32x_Chip_Erase()
 {
 	SPI_CSS_Low(QH32);
-	SPI_Send_Data(QH32, 0x06); //Write Enable
+	SPI_Send_Data(QH32, 0x60);
+	SPI_CSS_High(QH32);
+
+
+}
+
+void QH32x_Erase_Sector(uint32_t address)
+{
+	QH32x_Flash_Write_Enable();
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, 0x20);
+	SPI_Send_Data(QH32,(address & 0xff0000) >> 16);
+	SPI_Send_Data(QH32,((address & 0xFF00) >> 8));
+	SPI_Send_Data(QH32,((address & 0xFF)));
+	SPI_CSS_High(QH32);
+}
+
+
+
+void QH32x_Write_Flash(uint32_t addr, uint8_t data)
+{
+	QH32x_Flash_Write_Enable();
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, Page_Program);
+	SPI_Send_Data(QH32,((addr & 0xFF0000) >> 16));
+	SPI_Send_Data(QH32,((addr & 0xFF00) >> 8));
+	SPI_Send_Data(QH32,((addr & 0xFF)));
+	SPI_Send_Data(QH32, data);
 	SPI_CSS_High(QH32);
 
 }
-void QH32x_Write_Flash(uint32_t addr, uint8_t data);
-void QH32x_Write_Flash_Buffer(uint32_t addr, int len, uint8_t *data);
 
-void QH32x_Read_Flash(uint32_t addr);
-void QH32x_Read_Flash_Buffer(uint32_t addr, int len, uint8_t *data);
+void QH32x_Write_Flash_Buffer(uint32_t addr, int len, char *data)
+{
+	QH32x_Flash_Write_Enable(); //write enable
+	SPI_CSS_Low(QH32); //
+	SPI_Send_Data(QH32, 0x02); //PAGE PROGRAM
+	SPI_Send_Data(QH32,((addr & 0xFF0000) >> 16));
+	SPI_Send_Data(QH32,((addr & 0xFF00) >> 8));
+	SPI_Send_Data(QH32,((addr & 0xFF)));
+	for(int i = 0; i < len; i++)
+	{
+		SPI_Send_Data(QH32, data[i]);
+	}
+	SPI_CSS_High(QH32);
+
+}
+
+uint8_t QH32x_Read_Flash(uint32_t addr)
+{
+	while(QH32x_Read_Busy_Bit() == 1);
+	uint8_t x[2];
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, 0x03);
+	SPI_Send_Data(QH32,((addr & 0xFF0000) >> 16));
+	SPI_Send_Data(QH32,((addr & 0xFF00) >> 8));
+	SPI_Send_Data(QH32,((addr & 0xFF)));
+	x[0] = SPI_Receive_Data(QH32);
+	x[1] = SPI_Receive_Data(QH32);
+	SPI_CSS_High(QH32);
+
+	return x[1];
+}
+
+
+void QH32x_Read_Flash_Buffer(uint32_t addr, int len, uint8_t data[])
+{
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, 0x03);
+	SPI_Send_Data(QH32,((addr & 0xFF0000) >> 16));
+	SPI_Send_Data(QH32,((addr & 0xFF00) >> 8));
+	SPI_Send_Data(QH32,((addr & 0xFF)));
+	for(int i = 0; i < len; i++)
+	{
+		data[i] = SPI_Receive_Data(QH32);
+	}
+	SPI_CSS_High(QH32);
+}
+
+
+uint32_t QH32x_Read_ID(void)
+{
+	uint8_t x[6];
+	SPI_CSS_Low(QH32);
+	SPI_Send_Data(QH32, 0x9f); //9F
+	x[0] = SPI_Receive_Data(QH32);
+	x[1] = SPI_Receive_Data(QH32);
+	x[2] = SPI_Receive_Data(QH32);
+	SPI_CSS_High(QH32);
+	return (x[0] << 16) | (x[1] << 8) | x[0];
+}
 
 
 
-void QH32x_Test();
-
+void QH32x_Erase_Data(uint32_t start_address, uint32_t end_address)
+{
+	uint32_t diff;
+	uint8_t data[255];
+	if(end_address == 0)
+	{
+		QH32x_Write_Flash(start_address, 0x00);
+	}
+	else
+	{
+		diff = end_address - start_address;
+		uint32_t i;
+		for(i = 0; i < diff; i++)
+		{
+			data[i] = 0xFF;
+		}
+	}
+}
