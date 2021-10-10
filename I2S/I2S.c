50591c547b7439b61005249dbfd91da747000beb
Kunal Salvi <kunalsalvi63@gmail.com>
2021-10-10 22:45:56 +0530

    Update I2S.c

--- I2S/I2S.c
+++ I2S/I2S.c
@@ -1,116 +1,76 @@
 /*
- * I2S.c
+ * I2S.h
  *
  *  Created on: 28-Jun-2021
- *  Updated on: 15-Sep-2021
  *      Author: Kunal
  */
 
+#ifndef I2S_I2S_H_
+#define I2S_I2S_H_
 
-#include "I2S.h"
+#include "main.h"
+#include "stdbool.h"
+#include "GPIO/GPIO.h"
 
+#define I2S1 SPI1
+#define I2S2 SPI2
+#define I2S3 SPI3
+#define I2S4 SPI4
+#define I2S5 SPI5
 
-void I2S_Master_Init(I2S_Config I2S)
+#define I2S_Slave_TX 		0
+#define I2S_Slave_RX 	 	1
+#define I2S_Master_TX    	2
+#define I2S_Master_RX 	 	3
+
+#define  Philips_Standard	 0
+#define  MSB_Justified		 1
+#define  LSB_Justified		 2
+#define  PCM_Standard		 3
+
+#define Frame_16bit	         0
+#define Frame_24bit	         1
+#define Frame_32bit	         2
+
+#define Channel_16bit        0
+#define Channel_32bit        1
+
+#define I2S_DMA 0
+
+typedef struct I2S_Config
 {
-	int x;
-	I2S_Clock_Init();
-	if (I2S.I2S == I2S1)
-	{
-		RCC -> APB2ENR |= RCC_APB2ENR_SPI1EN;
-		GPIO_Pin_Setup('A', 4, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S1_WS);
-		GPIO_Pin_Setup('A', 5, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S1_CK);
-		GPIO_Pin_Setup('A', 7, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S1_SD);
+	SPI_TypeDef *I2S;
+	uint8_t mode;
+	uint8_t standard;
+	uint8_t data_length;
+	uint8_t channel_length;
+	int32_t audio_frequency;
+	bool RX_Interrupt;
+	bool TX_Interrupt;
 
-	}
-	if(I2S.I2S == I2S2)	//CK -> PB10 	//WS -> PB12 	//SD -> PB15
-	{
-		RCC -> APB1ENR |= RCC_APB1ENR_SPI2EN;
-        GPIO_Pin_Setup('B', 10, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S2_CK);
-		GPIO_Pin_Setup('B', 12, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S2_WS);
-		GPIO_Pin_Setup('B', 15, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S2_SD);
-	}
-	if(I2S.I2S == I2S3)	//CK -> PB12 	//WS -> PA15 	//SD -> PB5
-	{
-		RCC -> APB1ENR |= RCC_APB1ENR_SPI3EN;
-		GPIO_Pin_Setup('B', 12, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S3_CK);
-		GPIO_Pin_Setup('A', 15, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S3_WS);
-		GPIO_Pin_Setup('B', 5, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S3_SD);
-	}
-	if(I2S.I2S == I2S4)	//CK -> PB13 	//WS -> PB12 	//SD -> PA1
-	{
-		RCC -> APB2ENR |= RCC_APB2ENR_SPI4EN;
-		GPIO_Pin_Setup('A', 1,  ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S4_SD);
-		GPIO_Pin_Setup('B', 12, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S4_WS);
-		GPIO_Pin_Setup('B', 13, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S4_CK);
-	}
-	if(I2S.I2S == I2S5)	//CK -> PB0 	//WS -> PB1 	//SD -> PA10
-	{
-		RCC -> APB2ENR |= RCC_APB2ENR_SPI5EN;
-		GPIO_Pin_Setup('B', 0, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S5_CK);
-		GPIO_Pin_Setup('B', 1, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S5_WS);
-		GPIO_Pin_Setup('A', 10, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2S5_SD);
-	}
-	I2S.I2S -> I2SCFGR |= (SPI_I2SCFGR_I2SMOD |
-			               (I2S.mode << 8) |
-						   (I2S.standard << 4) |
-						   (I2S.data_length << 1) |
-						   (I2S.channel_length << 0));
-
-	if (I2S.channel_length == Channel_16bit)
-	{
-		x =(int)round(((172000000)/((16 * 2) * (2 * I2S.audio_frequency))));
-		x = x + 1;
-		I2S.I2S ->I2SPR = x;
-	}
-	else if (I2S.channel_length == Channel_32bit)
-	{
-		x =(int)(((172000000)/((32 * 2) * (2 * I2S.audio_frequency))));
-		x = x + 1;
-		I2S.I2S ->I2SPR = x;
-	}
-	I2S.I2S -> I2SPR |= 1 << 8; //ODD
-
-	if( I2S.RX_Interrupt == 1 ) I2S.I2S -> CR2 |= SPI_CR2_RXNEIE;
-	else I2S.I2S -> CR2 &= ~SPI_CR2_RXNEIE;
-
-	if( I2S.TX_Interrupt == 1 ) I2S.I2S -> CR2 |= SPI_CR2_TXEIE;
-	else I2S.I2S -> CR2 &= ~SPI_CR2_TXEIE;
-
-	I2S.I2S -> I2SCFGR |= SPI_I2SCFGR_I2SE;
-}
-
-void I2S_Master_Shut_Down(I2S_Config I2S)
-{
-	I2S.I2S ->I2SCFGR &= ~SPI_I2SCFGR_I2SMOD;
-}
-
-int16_t I2S_Master_Receive_Left_Channel(I2S_Config I2S)
-{
-	while(!(I2S.I2S-> SR & SPI_SR_CHSIDE)){}
-	return  (I2S.I2S -> DR);
-
-}
-
-int16_t I2S_Master_Receive_Right_Channel(I2S_Config I2S)
-{
-	while((I2S.I2S-> SR & SPI_SR_CHSIDE)){}
-	return  (I2S.I2S -> DR);
-}
+	bool RX_DMA;
+	bool TX_DMA;
+	uint32_t * data;
+	int length;
 
 
-void I2S_Master_Transmit_Left_Channel(I2S_Config I2S, int16_t data)
-{
-
-	while((I2S.I2S-> SR & SPI_SR_CHSIDE)){}
-	I2S.I2S -> DR = data;
+}I2S_Config;
 
 
-}
 
-void I2S_Master_Transmit_Right_Channel(I2S_Config I2S, int16_t data)
-{
-	while(!(I2S.I2S-> SR & SPI_SR_CHSIDE)){}
-	I2S.I2S -> DR = data;
+void I2S_Master_Init(I2S_Config I2S);
 
-}
+void I2S_Master_Start(I2S_Config I2S);
 
+void I2S_Master_Shut_Down(I2S_Config I2S);
+
+int16_t I2S_Master_Receive_Left_Channel(I2S_Config I2S);
+
+int16_t I2S_Master_Receive_Right_Channel(I2S_Config I2S);
+
+void I2S_Master_Transmit_Left_Channel(I2S_Config I2S, int16_t data);
+
+void I2S_Master_Transmit_Right_Channel(I2S_Config I2S, int16_t data);
+
+
+#endif /* I2S_I2S_H_ */
