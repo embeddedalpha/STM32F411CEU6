e099cb5259235c7f19a70a8d9e2a571ae83bf400
Kunal Salvi <kunalsalvi63@gmail.com>
2021-10-21 14:43:03 +0530

    Update WS2812B.c

--- Devices/WS2812B/WS2812B.c
+++ Devices/WS2812B/WS2812B.c
@@ -1,38 +1,70 @@
+/*
+ * WS2812B.c
+ *
+ *  Created on: 21-Oct-2021
+ *      Author: Kunal
+ */
+
+
 #include "WS2812B.h"
 
 
-void WS2811_Init()
+
+void WS2812B_Init(SPI_TypeDef *SPI)
 {
-   X.SPI = SPI1;
-   X.phase = 1;
-   X.polarity = 1;
-   X.Frame = SPI_Frame_8Bit;
-   X.LSB_MSB = 0;
-   X.DMA_TX = 0;
-   X.DMA_RX = 0;
-   X.frequency = 2;
-   X.mode = SPI_Master;
-   X.type = SPI_Full_Duplex_Master;
-   SPI_Init(X);
-   SPI_CSS_Init(X);
+	WS2812BSPI = SPI;
+	WS2812Bphase = 1;
+	WS2812Bpolarity = 1;
+	WS2812BFrame = SPI_Frame_8Bit;
+	WS2812BLSB_MSB = 0;
+	WS2812BDMA_TWS2812B = 0;
+	WS2812BDMA_RWS2812B = 0;
+	WS2812Bfrequency = 2;
+	WS2812Bmode = SPI_Master;
+	WS2812Btype = SPI_Full_DupleWS2812B_Master;
+	SPI_Init(WS2812B);
+	SPI_CSS_Init(WS2812B);
 }
 
 
-void WS2811_Send(int led_no, int red, int blue, int green)
+void WS2812B_Set_Color(int led_no, int red, int green, int blue)
 {
-	uint32_t color = green << 16 | red << 8 | blue;
-	SPI_CSS_Low(X);
-	for(int i = 24; i >= 0; i--)
+	uint32_t color  = green << 16 | red << 8 | blue;
+	led_color[len_no][color];
+
+}
+
+
+void WS2812B_Send_Data(uint32_t *led_color)
+{
+	int length = sizeof(led_color)/sizeof(led_color[0]);
+	for(int i = 0; i< length; i++)
 	{
-		if(color & (1 << i))
+		for(int j = 24; j >= 0; j--)
 		{
-			SPI_Send_Data(X, active_high);
-		}
-		else{
-			SPI_Send_Data(X, active_low);
+			if(led_color[i][j] & (1 << j))
+			{
+				SPI_Send_Data(WS2812B, active_high);
+			}
+			else{
+				SPI_Send_Data(WS2812B, active_low);
+			}
 		}
 	}
-	SPI_CSS_High(X);
-//	Delay_us(50);
 }
+
+
+
+void WS2812B_Clear_Strip(void)
+{
+	uint32_t color = 0x000000;
+	for(int i = 0; i< MAX_LEDS; i++)
+	{
+		for(int j = 24; j >= 0; j--)
+		{
+				SPI_Send_Data(WS2812B, active_low);
+		}
+	}
+}
+
 
