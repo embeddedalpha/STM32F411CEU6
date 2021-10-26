09879b56dbe2813420d7762f1cb4d136c7519a1c
Kunal Salvi <kunalsalvi63@gmail.com>
2021-10-26 23:56:39 +0530

    Update Console.h

--- Console/Console.h
+++ Console/Console.h
@@ -1,1 +1,27 @@
+/*
+ * Console.h
+ *
+ *  Created on: 25-Oct-2021
+ *      Author: Kunal
+ */
+
+#ifndef CONSOLE_CONSOLE_H_
+#define CONSOLE_CONSOLE_H_
+
+#include "main.h"
+#include "GPIO/GPIO.h"
+
+
+  void Console_Init(USART_TypeDef *port,int baudrate);
+  void printConsole(USART_TypeDef *port,char *msg, ...);
+  int scanConsole(char *msg,...);
+  char * Console_Return(char *s);
+
+
+
+
+
+
+
+#endif /* CONSOLE_CONSOLE_H_ */
 
