0407b3321d4243783e6411ce966dd94ab40a49b4
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-05 00:36:41 +0530

    Update I2C.c

--- I2C/I2C.c
+++ I2C/I2C.c
@@ -6,6 +6,7 @@
  *      Author: Kunal
  */
 
+
 #include "I2C.h"
 
 
@@ -14,8 +15,8 @@ int I2C_Master_Init(I2C_Config I2C)
 	if (I2C.I2C == I2C1)
 	{
 		RCC -> APB1ENR |= RCC_APB1ENR_I2C1EN;
-		GPIO_Pin_Setup('B', 6, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2C1_SCL);
-		GPIO_Pin_Setup('B', 7, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL, I2C1_SDA);
+		GPIO_Pin_Setup('B', 6, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN, I2C1_SCL);
+		GPIO_Pin_Setup('B', 7, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN, I2C1_SDA);
 
 	}
 	if (I2C.I2C == I2C2) //PB3 -> SDA	PB4 -> SDA
@@ -23,8 +24,8 @@ int I2C_Master_Init(I2C_Config I2C)
 		//PB3 -> SDA
 		//PB4 -> SDA
 		RCC -> APB1ENR |= RCC_APB1ENR_I2C2EN;
-		GPIO_Pin_Setup('B', 3, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL_PULLUP, I2C2_SCL);
-		GPIO_Pin_Setup('B', 4, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL_PULLUP, I2C2_SDA);
+		GPIO_Pin_Setup('B', 3, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN_PULLUP, I2C2_SCL);
+		GPIO_Pin_Setup('B', 4, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN_PULLUP, I2C2_SDA);
 
 	}
 	if (I2C.I2C == I2C3)
@@ -33,8 +34,8 @@ int I2C_Master_Init(I2C_Config I2C)
 		RCC -> AHB1ENR |= RCC_AHB1ENR_GPIOAEN | RCC_AHB1ENR_GPIOBEN;
 		//PA8 -> SDA
 		//PB10 -> SCL
-		GPIO_Pin_Setup('A', 8, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL_PULLUP, I2C3_SCL);
-		GPIO_Pin_Setup('B', 10, ALTERNATE_FUNCTION_OUTPUT_PUSHPULL_PULLUP, I2C3_SDA);
+		GPIO_Pin_Setup('A', 8, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN_PULLUP, I2C3_SCL);
+		GPIO_Pin_Setup('B', 10, ALTERNATE_FUNCTION_OUTPUT_OPENDRAIN_PULLUP, I2C3_SDA);
 	}
 
 	if (I2C.mode)
@@ -43,7 +44,7 @@ int I2C_Master_Init(I2C_Config I2C)
 		I2C.I2C -> CR1 |= I2C_CR1_SWRST;
 		I2C.I2C -> CR1 &= ~I2C_CR1_SWRST;
 
-		I2C.I2C -> OAR1 = 0x4000;
+//		I2C.I2C -> OAR1 = 0x4000;
 		I2C.I2C -> CR2 = 0x30; //48MHz
 		I2C.I2C -> CCR = 0x8028; //FS mode | 40
 		I2C.I2C -> TRISE = 0xf;
@@ -68,51 +69,77 @@ return 0;
 
 void I2C_Master_Start(I2C_Config I2C)
 {
-	uint16_t reg;
 	I2C.I2C -> CR1 &= ~I2C_CR1_POS;
 	I2C.I2C -> CR1 |= I2C_CR1_START;
 	while(!(I2C.I2C -> SR1 & I2C_SR1_SB)){}
-	 reg = I2C.I2C ->SR1;
+	CLEAR_REG(I2C.I2C -> SR1);
+	CLEAR_REG(I2C.I2C -> SR2);
 }
 
-void I2C_Master_Send_Address(I2C_Config I2C, char address)
+void I2C_Master_Send_Address(I2C_Config I2C, uint8_t address)
 {
-	uint16_t reg1;
-	I2C.I2C -> DR = (address << 1) | 0x00;
-	while(!(I2C.I2C -> SR1 & I2C_SR1_ADDR)){}
-	reg1 = 0x00;
-	reg1 = I2C.I2C -> SR1;
-	reg1 = I2C.I2C -> SR2;
-	while(!(I2C.I2C -> SR1 & I2C_SR1_TXE)){}
+	int reg;
+	do {
+		I2C.I2C -> DR = (address) << 1 | 0x00;
+		reg = (I2C.I2C -> SR1 & (1 << 1) )>> 1;
+	} while (reg == 0);
 
+	int i;
+	do {
+		reg = I2C.I2C -> SR1;
+		reg = I2C.I2C -> SR2;
+		reg = 0;
+		reg = (I2C.I2C -> SR1 & (1 << 1) )>> 1;
+		i++;
+	} while (reg != 0 || i < 100);
+	i = 0;
 }
 
 void I2C_Master_Receive_Address(I2C_Config I2C, char address)
 {
-//	I2C.I2C -> CR1 |= I2C_CR1_ACK;
+
 	I2C.I2C -> DR = (address << 1) | 0x01;
-	while(!(I2C.I2C->SR1 & I2C_SR1_ADDR)){}
+
+	while((I2C.I2C->SR1 & I2C_SR1_ADDR) != I2C_SR1_ADDR){}
 	reg1 = I2C.I2C -> SR1;
 	reg2 = I2C.I2C -> SR2;
 
 }
 
-void I2C_Master_Send_Data(I2C_Config I2C, char data)
+void I2C_Master_Send_Data(I2C_Config I2C, uint8_t data)
 {
-	I2C.I2C -> DR = data;
-	while(!(I2C.I2C->SR1 & I2C_SR1_TXE));
+	CLEAR_REG(I2C.I2C -> SR1);
+	CLEAR_REG(I2C.I2C -> SR2);
+int reg;
+	do {
+		I2C.I2C -> DR = data;
+			I2C.I2C -> CR1 |= I2C_CR1_ACK;
+		reg = (I2C.I2C -> SR1 & I2C_SR1_TXE) >> 7;
+	} while (reg == 0);
+	int i;
+	do {
+		I2C.I2C -> SR1 = 0;
+		I2C.I2C -> SR2 = 0;
+		reg = I2C.I2C -> SR1;
+		reg = I2C.I2C -> SR2;
+		reg = 0;
+		reg = (I2C.I2C -> SR1 & I2C_SR1_TXE )>> 7;
+		i++;
+	} while (reg != 0 && i < 100);
+	i = 0;
+
 }
 
 char I2C_Master_Receive_Data(I2C_Config I2C)
 {
 //	I2C.I2C -> CR1 |= I2C_CR1_POS;
-	I2C.I2C -> CR1 |= I2C_CR1_ACK;
+//	I2C.I2C -> CR1 |= I2C_CR1_ACK;
 	char temp = 0;
 //	while(!(I2C.I2C->SR1 & I2C_SR1_BTF));
 //	I2C.I2C -> CR1 |= I2C_CR1_STOP;
 //	temp = I2C.I2C -> DR;
 //	I2C.I2C -> CR1 |= I2C_CR1_ACK;
-	while(!(I2C.I2C -> SR1 & I2C_SR1_RXNE)){}
+//	while(!(I2C.I2C -> SR1 & I2C_SR1_RXNE)){}
 	temp = I2C.I2C -> DR;
 	I2C.I2C -> CR1 |= I2C_CR1_ACK;
 	return temp;
