1198879136e652ed935db82177737013dce5d46b
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-05 00:36:59 +0530

    Update I2C.h

--- I2C/I2C.h
+++ I2C/I2C.h
@@ -11,11 +11,11 @@
 #include "stm32f4xx.h"
 #include "stm32f411xe.h"
 #include "stdbool.h"
+#include "GPIO/GPIO.h"
 
 #define Fast_Mode	1
 #define Normal_Mode	0
-#define read 0
-#define write 1
+
 
 uint16_t reg1, reg2;
 
@@ -31,18 +31,16 @@ int I2C_Master_Init(I2C_Config I2C);
 
 void I2C_Master_Start(I2C_Config I2C);
 
-void I2C_Master_Send_Address(I2C_Config I2C, char address);
+void I2C_Master_Send_Address(I2C_Config I2C, uint8_t address);
 
-void I2C_Master_Send_Data(I2C_Config I2C, char data);
+void I2C_Master_Receive_Address(I2C_Config I2C, char address);
+
+void I2C_Master_Send_Data(I2C_Config I2C, uint8_t data);
 
 char I2C_Master_Receive_Data(I2C_Config I2C);
 
 void I2C_Master_Stop(I2C_Config I2C);
 
 void I2C_Master_Send_NACK(I2C_Config I2C);
-
-void I2C_Send_Buffer(I2C_Config I2C, uint8_t address, uint8_t *buffer, int length);
-
-void I2C_Receive_Buffer(I2C_Config I2C, uint8_t address, uint8_t *buffer, int length);
 
 #endif /* I2C_I2C_H_ */
