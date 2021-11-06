f90fdeda3507929b4348b4a91516fc5312c44866
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-06 23:27:46 +0530

    Update DMA.c

--- DMA/DMA.c
+++ DMA/DMA.c
@@ -10,116 +10,126 @@
 
 void DMA_Init(DMA_Config DMA)
 {
+	if(DMA.DMA_Stream == DMA1_Stream0 ||
+	   DMA.DMA_Stream == DMA1_Stream1 ||
+	   DMA.DMA_Stream == DMA1_Stream2 ||
+	   DMA.DMA_Stream == DMA1_Stream3 ||
+	   DMA.DMA_Stream == DMA1_Stream4 ||
+	   DMA.DMA_Stream == DMA1_Stream5 ||
+	   DMA.DMA_Stream == DMA1_Stream6 )
+	{
+		RCC->AHB1ENR |= RCC_AHB1ENR_DMA1EN;
+	}
+	if(DMA.DMA_Stream == DMA2_Stream0 ||
+	   DMA.DMA_Stream == DMA2_Stream1 ||
+	   DMA.DMA_Stream == DMA2_Stream2 ||
+	   DMA.DMA_Stream == DMA2_Stream3 ||
+	   DMA.DMA_Stream == DMA2_Stream4 ||
+	   DMA.DMA_Stream == DMA2_Stream5 ||
+	   DMA.DMA_Stream == DMA2_Stream6 )
+	{
+		RCC->AHB1ENR |= RCC_AHB1ENR_DMA2EN;
+	}
+
 	DMA.DMA_Stream -> CR |= ( DMA.channel << 25 |
-			                  DMA.priority << 16 |
+			                  DMA.priority_level << 16 |
 							  DMA.memory_size << 13 |
 							  DMA.peripheral_size << 11 |
-							  1 << 10 | 1 << 8 | DMA.direction << 6 );
-	DMA.DMA_Stream -> NDTR = DMA.data_length;
+							  DMA.circular_mode << 8 |
+							  DMA.direction << 6 |
+							  1 << 8 |
+							  1 << 10
+							  );
+
+	DMA.DMA_Stream -> NDTR = DMA.packet_length;
 	DMA.DMA_Stream -> PAR = DMA.source_address;
 	DMA.DMA_Stream -> M0AR = DMA.destination_address;
+
+}
+
+void DMA_Interrupt(DMA_Config DMA)
+{
+	if(DMA.DMA_Stream == DMA1_Stream0)
+	{
+		NVIC_SetPriority(DMA1_Stream0_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream0_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream1)
+	{
+		NVIC_SetPriority(DMA1_Stream1_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream1_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream2)
+	{
+		NVIC_SetPriority(DMA1_Stream2_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream2_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream3)
+	{
+		NVIC_SetPriority(DMA1_Stream3_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream3_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream4)
+	{
+		NVIC_SetPriority(DMA1_Stream4_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream4_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream5)
+	{
+		NVIC_SetPriority(DMA1_Stream5_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream5_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA1_Stream6)
+	{
+		NVIC_SetPriority(DMA1_Stream6_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+	}
+/**************************************************************************************/
+	else 	if(DMA.DMA_Stream == DMA2_Stream0)
+	{
+		NVIC_SetPriority(DMA2_Stream0_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA1_Stream0_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream1)
+	{
+		NVIC_SetPriority(DMA2_Stream1_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream1_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream2)
+	{
+		NVIC_SetPriority(DMA2_Stream2_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream2_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream3)
+	{
+		NVIC_SetPriority(DMA2_Stream3_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream3_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream4)
+	{
+		NVIC_SetPriority(DMA2_Stream4_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream4_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream5)
+	{
+		NVIC_SetPriority(DMA2_Stream5_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream5_IRQn);
+	}
+	else if(DMA.DMA_Stream == DMA2_Stream6)
+	{
+		NVIC_SetPriority(DMA2_Stream6_IRQn,DMA.interrupt_priority);
+		NVIC_EnableIRQ(DMA2_Stream6_IRQn);
+	}
+/**************************************************************************************/
+	DMA.DMA_Stream -> CR |= DMA.transfer_complete_interrupt << 4 |
+			        DMA.half_transfer_interrupt << 3 |
+				DMA.transfer_error_interrupt << 2 |
+				DMA.direct_mode_error_interrupt << 1;
+}
+
+void DMA_Enable(DMA_Config DMA)
+{
 	DMA.DMA_Stream -> CR |= DMA_SxCR_EN;
-}
-
-
-void DMA_UART_TX_Init(USART_TypeDef *USART, char data[], int len)
-{
-	if(USART == USART1)
-	{
-		RCC -> AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-		DMA2_Stream7 -> CR |= ( 4 << 25 ) | (1 << 10) | (1 << 8) | (1 << 6) | (1 << 4) | (1 << 3) |
-				 (1 << 2) |  (1 << 1);
-		DMA2_Stream7 -> NDTR = len;
-		DMA2_Stream7 -> M0AR = (uint32_t)&data[0];
-		DMA2_Stream7 -> PAR = (uint32_t)&(USART1->DR);
-	}else if(USART == USART2)
-		{
-		RCC -> AHB1ENR |= RCC_AHB1ENR_DMA1EN;
-		DMA1_Stream6 -> CR |= ( 6 << 25 ) | (1 << 10) | (1 << 8) | (1 << 6) | (1 << 4) | (1 << 3) |
-				 (1 << 2) |  (1 << 1);
-		DMA1_Stream6 -> NDTR = len;
-		DMA1_Stream6 -> M0AR = (uint32_t)&data[0];
-		DMA1_Stream6 -> PAR = (uint32_t)&(USART2->DR);
-		}else if(USART == USART6)
-			{
-			RCC -> AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-			DMA2_Stream7 -> CR |= ( 5 << 25 ) | (1 << 10) | (1 << 8) | (1 << 6) | (1 << 4) | (1 << 3) |
-					 (1 << 2) |  (1 << 1);
-			DMA2_Stream7 -> NDTR = len;
-			DMA2_Stream7 -> M0AR = (uint32_t)&data[0];
-			DMA2_Stream7 -> PAR = (uint32_t)&(USART6->DR);
-			}
-}
-
-
-void DMA_UART_TX_Start(USART_TypeDef *USART)
-{
-	if(USART == USART1)
-	{
-		USART1 -> CR3 |= USART_CR3_DMAT;
-		USART1 -> CR1 |= USART_CR1_UE;
-		DMA2_Stream7 -> CR |= 1 << 0;
-	}else if(USART == USART2)
-		{
-		USART2 -> CR3 |= USART_CR3_DMAT;
-		USART2 -> CR1 |= USART_CR1_UE;
-		DMA1_Stream6 -> CR |= 1 << 0;
-		}else if(USART == USART6)
-			{
-			USART6 -> CR3 |= USART_CR3_DMAT;
-			USART6 -> CR1 |= USART_CR1_UE;
-			DMA2_Stream7 -> CR |= 1 << 0;
-			}
-}
-
-void DMA_UART_RX_Init(USART_TypeDef *USART, char data[], int len)
-{
-	if(USART == USART1)
-	{
-		RCC -> AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-		DMA2_Stream2 -> CR |= ( 4 << 25 ) | (1 << 10) | (1 << 8) | (0 << 6) | (1 << 4) | (1 << 3) |
-				 (1 << 2) |  (1 << 1);
-		DMA2_Stream2 -> NDTR = len;
-		DMA2_Stream2 -> M0AR = (uint32_t)&data[0];
-		DMA2_Stream2 -> PAR = (uint32_t)&(USART1->DR);
-	}else if(USART == USART2)
-		{
-		RCC -> AHB1ENR |= RCC_AHB1ENR_DMA1EN;
-		DMA1_Stream5 -> CR |= ( 5 << 25 ) | (1 << 10) | (1 << 8) | (0 << 6) | (1 << 4) | (1 << 3) |
-				 (1 << 2) |  (1 << 1);
-		DMA1_Stream5 -> NDTR = len;
-		DMA1_Stream5 -> M0AR = (uint32_t)&data[0];
-		DMA1_Stream5 -> PAR = (uint32_t)&(USART2->DR);
-		}else if(USART == USART6)
-			{
-			RCC -> AHB1ENR |= RCC_AHB1ENR_DMA2EN;
-			DMA2_Stream1 -> CR |= ( 5 << 25 ) | (1 << 10) | (1 << 8) | (0 << 6) | (1 << 4) | (1 << 3) |
-					 (1 << 2) |  (1 << 1);
-			DMA2_Stream1 -> NDTR = len;
-			DMA2_Stream1 -> M0AR = (uint32_t)&data[0];
-			DMA2_Stream1 -> PAR = (uint32_t)&(USART6->DR);
-			}
-}
-
-
-void DMA_UART_RX_Start(USART_TypeDef *USART)
-{
-	if(USART == USART1)
-	{
-		USART1 -> CR3 |= USART_CR3_DMAT;
-		USART1 -> CR1 |= USART_CR1_UE;
-		DMA2_Stream2 -> CR |= 1 << 0;
-	}else if(USART == USART2)
-		{
-		USART2 -> CR3 |= USART_CR3_DMAT;
-		USART2 -> CR1 |= USART_CR1_UE;
-		DMA1_Stream6 -> CR |= 1 << 0;
-		}else if(USART == USART6)
-			{
-			USART6 -> CR3 |= USART_CR3_DMAT;
-			USART6 -> CR1 |= USART_CR1_UE;
-			DMA2_Stream1 -> CR |= 1 << 0;
-			}
 }
 
 
