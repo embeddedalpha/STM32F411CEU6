b47075bd53c9c077d2b7fb0f2199ec10d4ec5f6f
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-22 00:25:57 +0530

    Update I2C.h

--- I2C/I2C.h
+++ I2C/I2C.h
@@ -8,39 +8,39 @@
 #ifndef I2C_I2C_H_
 #define I2C_I2C_H_
 
-#include "stm32f4xx.h"
-#include "stm32f411xe.h"
-#include "stdbool.h"
+#include "main.h"
 #include "GPIO/GPIO.h"
 
-#define Fast_Mode	1
-#define Normal_Mode	0
+#define I2C_Fast_Mode	1
+#define I2C_Normal_Mode	0
 
-
-uint16_t reg1, reg2;
 
 typedef struct I2C_Config
 {
 	I2C_TypeDef *I2C;
 	bool mode;
-
 }I2C_Config;
 
 
-int I2C_Master_Init(I2C_Config I2C);
+void I2C_Master_Init(I2C_Config I2C);
 
 void I2C_Master_Start(I2C_Config I2C);
 
-void I2C_Master_Send_Address(I2C_Config I2C, uint8_t address);
+void I2C_Master_Address(I2C_Config I2C, uint8_t address, uint8_t read_write);
 
-void I2C_Master_Receive_Address(I2C_Config I2C, char address);
+void I2C_Master_Send_Byte(I2C_Config I2C, uint8_t data);
 
-void I2C_Master_Send_Data(I2C_Config I2C, uint8_t data);
+void I2C_Master_Send_Buffer(I2C_Config I2C, uint8_t *data, int length);
 
-char I2C_Master_Receive_Data(I2C_Config I2C);
+void I2C_Master_Write_Register(I2C_Config I2C, uint8_t device_address, uint8_t reg_address, uint8_t data);
+
+int I2C_Master_Receive_Byte(I2C_Config I2C);
+
+void I2C_Master_NACK(I2C_Config I2C);
+
+void I2C_Master_ACK(I2C_Config I2C);
 
 void I2C_Master_Stop(I2C_Config I2C);
 
-void I2C_Master_Send_NACK(I2C_Config I2C);
 
 #endif /* I2C_I2C_H_ */
