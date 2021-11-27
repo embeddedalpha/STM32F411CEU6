9953fbfd062a0a6e146c33d3e96db2d3a1da5407
Kunal Salvi <kunalsalvi63@gmail.com>
2021-11-27 23:04:25 +0530

    Update SPI.h

--- SPI/SPI.h
+++ SPI/SPI.h
@@ -2,7 +2,6 @@
  * SPI.h
  *
  *  Created on: 18-Jul-2021
- *  Updated on: 03-Oct-2021
  *      Author: Kunal
  */
 
@@ -12,12 +11,13 @@
 #include "main.h"
 #include "GPIO/GPIO.h"
 
-#define Full_Duplex_Master    0
-#define Half_Duplex_Master_TX 1
-#define Half_Duplex_Master_RX 2
-#define Full_Duplex_Slave     3
-#define Half_Duplex_Slave_TX  4
-#define Half_Duplex_Slave_RX  5
+#define SPI_Full_Duplex_Master    0
+#define SPI_Half_Duplex_Master_TX 1
+#define SPI_Half_Duplex_Master_RX 2
+
+#define SPI_Full_Duplex_Slave     3
+#define SPI_Half_Duplex_Slave_TX  4
+#define SPI_Half_Duplex_Slave_RX  5
 
 #define SPI_Frame_8Bit 0
 #define SPI_Frame_16Bit 1
@@ -32,25 +32,25 @@ typedef struct SPI_Config
 {
 	SPI_TypeDef *SPI;
 	uint8_t type;
-	bool mode;
+	uint8_t mode;
 	int frequency;
-	bool phase;
-	bool polarity;
-	bool LSB_MSB;
-	bool CRC_Enable;
-	bool DMA_TX;
-	bool DMA_RX;
-	bool Frame;
+	uint8_t phase;
+	uint8_t polarity;
+	uint8_t LSB_MSB;
+	uint8_t CRC_Enable;
+	uint8_t DMA_TX;
+	uint8_t DMA_RX;
+	uint8_t Frame;
+	GPIO_TypeDef *nCSS_Port;
+	int nCSS_pin;
 }SPI_Config;
 
 void SPI_Init(SPI_Config SPI);
 void SPI_CSS_Init(SPI_Config SPI);
 void SPI_CSS_High(SPI_Config SPI);
 void SPI_CSS_Low(SPI_Config SPI);
-void SPI_Send_Data(SPI_Config SPI,int16_t data);
-int16_t SPI_Receive_Data(SPI_Config SPI);
-
-
+void SPI_Send_Data(SPI_Config SPI, int data);
+int SPI_Receive_Data(SPI_Config SPI);
 
 
 #endif /* SPI_SPI_H_ */
