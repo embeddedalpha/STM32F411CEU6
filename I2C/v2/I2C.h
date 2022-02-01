c897969e2564835c30e663129eec3c07eab4dd12
Kunal Salvi <kunalsalvi63@gmail.com>
2022-02-01 11:45:20 +0530

    Create I2C.h

---
+++ I2C/v2/I2C.h
@@ -0,0 +1,52 @@
+/*
+ * I2C.h
+ *
+ *  Created on: 08-Jul-2021
+ *      Author: Kunal
+ */
+
+#ifndef I2C_I2C_H_
+#define I2C_I2C_H_
+
+#include "main.h"
+#include "GPIO/GPIO.h"
+
+#define I2C_Fast_Mode	1
+#define I2C_Normal_Mode	0
+
+
+typedef struct I2C_Config
+{
+	I2C_TypeDef *I2C;
+	bool mode;
+}I2C_Config;
+
+
+void I2C_Master_Init(I2C_Config I2C);
+
+int I2C_Master_Start(I2C_Config I2C);
+
+void I2C_Master_Address(I2C_Config I2C, uint8_t address, uint8_t read_write);
+
+void I2C_Master_Send_Byte(I2C_Config I2C, uint8_t data);
+
+void I2C_Master_Send_Buffer(I2C_Config I2C, uint8_t *data, int length);
+
+void I2C_Master_Write_Register(I2C_Config I2C, uint8_t device_address, uint8_t reg_address, uint8_t data);
+
+int I2C_Master_Read_Register(I2C_Config I2C, uint8_t device_address, uint8_t reg_address);
+
+int I2C_Master_Receive_Byte(I2C_Config I2C);
+
+void I2C_Master_NACK(I2C_Config I2C);
+
+void I2C_Master_ACK(I2C_Config I2C);
+
+void I2C_Master_Stop(I2C_Config I2C);
+
+void I2C_Master_Write_Register_bits(I2C_Config I2C,uint8_t device_address, uint8_t reg_address, uint8_t SET_RESET_FLAG, uint8_t bits);
+
+int I2C_Master_Read_Register_bit(I2C_Config I2C, uint8_t device_address, uint8_t reg_address, uint8_t bit);
+
+
+#endif /* I2C_I2C_H_ */
